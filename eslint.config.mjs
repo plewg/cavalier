d9d161fe7b35550a35c95694677a74f3fa464504
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-03-19 13:16:52 -0400

    [update_packages] update node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const config = defineConfig([
     {
         settings: {
             // NOTE: required because n plugin doesn't read the root package.json
-            node: { version: "^22" },
+            node: { version: "^24" },
         },
     },
     pentible,
