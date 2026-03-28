368679c052eb431f408896a561e332a1ddd1d2e5
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-03-27 20:05:47 -0400

    [videos-screen] fix incorrect behaviour on final page

--- apps/web/src/routers/video.ts
+++ apps/web/src/routers/video.ts
@@ -51,7 +51,14 @@ export const videoRouter = createTrpcRouter({
                 orderBy: [{ publishedAt: sortDirection }, { id: "asc" }],
             });
 
-            const nextCursor = videos[videos.length - 1]?.id;
+            // We grab the item at PAGE_SIZE index, rather than the item at the
+            // last index, because the final page may not be a full page, and we
+            // want to return `undefined` in that case to indicate that there is
+            // not an additional page. If we instead grabbed the last item it
+            // would incorrectly provide a cursor for another page that contains
+            // only that final video, and the same cursor (theoretically
+            // creating an infinite loop, albeit one dependent on user action).
+            const nextCursor = videos[PAGE_SIZE]?.id;
 
             return { videos: videos.slice(0, PAGE_SIZE), nextCursor };
         }),
