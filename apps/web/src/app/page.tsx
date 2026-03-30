fc57899fa0ac13f0ae4743955861437cade33bbf
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-03-30 17:20:19 -0400

    [profile-page] initial profile page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Menu } from "@base-ui/react";
 import {
     useInfiniteQuery,
     useMutation,
@@ -7,8 +8,9 @@ import {
     useQueryClient,
 } from "@tanstack/react-query";
 import { Duration } from "luxon";
+import Link from "next/link";
 import { useState } from "react";
-import { FiExternalLink } from "react-icons/fi";
+import { FiArrowDown, FiArrowUp, FiExternalLink, FiMenu } from "react-icons/fi";
 import type { RouterOutputs } from "#src/routers/root";
 import { useTrpc } from "#src/trpc/react";
 import { UnreachableError } from "#src/utils/errors";
@@ -20,13 +22,17 @@ export default function Home() {
     );
 
     if (isLoading || session === undefined) {
-        return <div>Loading</div>;
+        return (
+            <div className="flex h-full w-full items-center justify-center">
+                Loading
+            </div>
+        );
     }
 
     return (
         <main className="flex min-h-full w-full flex-col items-center justify-start gap-10 p-4">
-            <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
-                <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-center text-4xl font-extrabold text-transparent lg:self-start">
+            <div className="flex w-full flex-row items-center justify-between gap-2">
+                <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-center text-4xl font-extrabold text-transparent">
                     CAVALIER
                 </div>
                 {session === null ? (
@@ -37,25 +43,58 @@ export default function Home() {
                         Log In
                     </a>
                 ) : (
-                    <div className="flex flex-col-reverse items-center gap-2 lg:flex-row lg:justify-end">
-                        {session.user.watchLaterPlaylistId !== null ? (
-                            <a
-                                className="flex flex-row items-center gap-1"
-                                href={`https://youtube.com/playlist?list=${session.user.watchLaterPlaylistId}`}
-                                target="_blank"
-                                rel="noreferrer"
+                    <Menu.Root>
+                        <Menu.Trigger
+                            render={
+                                <button
+                                    className="flex justify-center self-center rounded-md border-2 border-gray-600 p-3 text-center lg:self-end"
+                                    type="button"
+                                >
+                                    <FiMenu />
+                                </button>
+                            }
+                        />
+                        <Menu.Portal>
+                            <Menu.Positioner
+                                align="end"
+                                className="w-48 rounded-md border border-gray-500 bg-gray-900 p-1"
                             >
-                                View Playlist
-                                <FiExternalLink />
-                            </a>
-                        ) : undefined}
-                        <a
-                            className="flex w-48 items-center justify-center rounded-md border-2 border-green-600 bg-green-700 px-2 py-3"
-                            href="/api/auth/logout"
-                        >
-                            Log Out
-                        </a>
-                    </div>
+                                <Menu.Popup className="p-1">
+                                    {session.user.watchLaterPlaylistId !==
+                                    null ? (
+                                        <div>
+                                            <Menu.Group>
+                                                <Menu.Item>
+                                                    <a
+                                                        href={`https://youtube.com/playlist?list=${session.user.watchLaterPlaylistId}`}
+                                                        className="flex flex-row items-center gap-1"
+                                                        target="_blank"
+                                                        rel="noreferrer"
+                                                    >
+                                                        View Playlist
+                                                        <FiExternalLink />
+                                                    </a>
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    <Link href="/profile">
+                                                        Profile
+                                                    </Link>
+                                                </Menu.Item>
+                                            </Menu.Group>
+                                            <Menu.Separator className="my-1 h-[1] bg-gray-200" />
+                                        </div>
+                                    ) : undefined}
+                                    <Menu.Group>
+                                        <Menu.Item>
+                                            <a href="/api/auth/logout">
+                                                Log Out
+                                            </a>
+                                        </Menu.Item>
+                                    </Menu.Group>
+                                </Menu.Popup>
+                            </Menu.Positioner>
+                        </Menu.Portal>
+                    </Menu.Root>
                 )}
             </div>
             {session !== null ? <VideoScreen /> : undefined}
@@ -121,11 +160,9 @@ function VideoScreen() {
                 onClick={() => {
                     setSortDirection(sortDirection === "asc" ? "desc" : "asc");
                 }}
-                className="flex w-48 justify-center self-center rounded-md border-2 border-gray-600 p-3 text-center lg:self-end"
+                className="flex justify-center self-end rounded-md border-2 border-gray-600 p-3 text-center"
             >
-                {sortDirection === "asc"
-                    ? "Sort Oldest First"
-                    : "Sort Newest First"}
+                {sortDirection === "asc" ? <FiArrowUp /> : <FiArrowDown />}
             </button>
             <div className="grid max-w-7xl grid-cols-1 gap-3 gap-y-8 pb-[100vh] lg:grid-cols-4 xl:grid-cols-5">
                 {data.pages.map(({ videos }) => {
