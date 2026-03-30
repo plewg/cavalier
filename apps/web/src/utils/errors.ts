030f90d0693c6724c0e71925e4be6d4a90265976
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-03-30 19:35:41 -0400

    [fix-logout] i sure hope this fixes it because this is dumb as fuck

--- apps/web/src/utils/errors.ts
+++ apps/web/src/utils/errors.ts
@@ -1,3 +1,7 @@
 export class UnreachableError extends Error {
     override name = "UnreachableError";
 }
+
+export function isErrorWithCode(error: unknown) {
+    return error !== null && typeof error === "object" && "code" in error;
+}
