511a78237c8c19d7ddcc3f3cea25dc4d9f85ee7d
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-04-02 10:18:58 -0400

    [animate-mutation-done-icon] don't show settled state on first render, fade it out after interacting with a video

--- apps/web/src/app/the-syncler.tsx
+++ apps/web/src/app/the-syncler.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMutationState } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { FiActivity, FiAlertOctagon, FiCheckCircle } from "react-icons/fi";
 
 export function TheSyncler() {
@@ -11,6 +12,14 @@ export function TheSyncler() {
     const erroredMutations = useMutationState({
         filters: { status: "error" },
     });
+
+    const [isFirst, setIsFirst] = useState(true);
+
+    useEffect(() => {
+        if (isFirst && activeMutationCount > 0) {
+            setIsFirst(false);
+        }
+    }, [activeMutationCount, isFirst]);
 
     if (erroredMutations.length > 0) {
         const message = erroredMutations.map((m) => m.failureReason).join(", ");
@@ -34,7 +43,13 @@ export function TheSyncler() {
                     className="animate-pulse text-yellow-500"
                 />
             ) : (
-                <FiCheckCircle size="22px" className="text-green-400" />
+                <FiCheckCircle
+                    size="22px"
+                    className="text-green-400 opacity-0"
+                    style={{
+                        animation: isFirst ? undefined : "fadeout 1500ms",
+                    }}
+                />
             )}
         </div>
     );
