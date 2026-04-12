5cdd143ce40cbbcd244e23ed51c2daaca310b579
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-04-11 21:02:32 -0400

    [import-watch-history] handle nonextant videos, tidy up

--- apps/web/src/utils/array.ts
+++ apps/web/src/utils/array.ts
@@ -33,6 +33,42 @@ export async function asyncForEach<T>(
     await Promise.all(promises);
 }
 
+export async function asyncMap<T, U>(
+    iterable: AsyncIterable<T> | Iterable<T>,
+    maxConcurrent: number,
+    callback: (element: T) => Promise<U>,
+) {
+    const results: U[] = [];
+    const promises = new Set<Promise<void>>();
+
+    let index = 0;
+    for await (const element of iterable) {
+        const currentIndex = index++;
+
+        const promise = callback(element)
+            .then((u) => {
+                // NOTE: store at index instead of push to preserve order
+                results[currentIndex] = u;
+            })
+            .finally(() => {
+                // remove current promise from promises
+                promises.delete(promise);
+            });
+
+        promises.add(promise);
+
+        if (promises.size >= maxConcurrent) {
+            // wait for any promise to finish before adding more
+            await Promise.race(promises);
+        }
+    }
+
+    // wait for remaining promises to finish
+    await Promise.all(promises);
+
+    return results;
+}
+
 export function unique<T>(element: T, index: number, array: T[]) {
     return array.indexOf(element) === index;
 }
