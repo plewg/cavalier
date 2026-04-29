4a8da0346cc6deaa8a5e08dc08a08308e8fb8ee5
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-04-29 10:45:02 -0400

    [logged-out-redirect] set redirectTo when redirected from a protected route due to not being authenticated

--- apps/web/src/app/api/auth/login/route.ts
+++ apps/web/src/app/api/auth/login/route.ts
@@ -1,13 +1,19 @@
+import { Buffer } from "node:buffer";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { createGoogleClient } from "#src/youtube/google";
 
-export function GET() {
+export function GET(request: NextRequest) {
+    const redirectTo = request.nextUrl.searchParams.get("redirectTo") ?? "";
+    const encodedRedirectTo = Buffer.from(redirectTo).toString("base64url");
+
     const client = createGoogleClient();
     const scopes = ["https://www.googleapis.com/auth/youtube", "openid"];
     const url = client.generateAuthUrl({
         scope: scopes,
         prompt: "consent",
         access_type: "offline",
+        state: encodedRedirectTo,
     });
 
     return NextResponse.redirect(url);
